846f6c304d537b1d49523555193734b29f52fe16
AM_Workstation <arkhi07@hotmail.co.id>
2019-02-25 05:05:05 +0700

    Last

--- app/src/main/java/com/DetechtiveCode/aplikasiaiss/QuizActivity.java
+++ app/src/main/java/com/DetechtiveCode/aplikasiaiss/QuizActivity.java
@@ -1,12 +1,17 @@
 package com.DetechtiveCode.aplikasiaiss;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +22,16 @@ import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
     RadioGroup mrgPilihanJawaban;
+    Button kusi,tutup;
     RadioButton mrbPilihanJawaban1, mrbPilihanJawaban2, mrbPilihanJawaban3;
     private TextView tvKuis;
-    private String jawabanBenar;
+    private String jawaban;
     private SoalPilihanGanda pertanyaan = new SoalPilihanGanda();
     private int pertanyaanlength = pertanyaan.pertanyaan.length;
     Random r;
+    Dialog Dialogku, Dialognya;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,53 +42,116 @@ public class QuizActivity extends AppCompatActivity {
         mrbPilihanJawaban2 = findViewById(R.id.rbPilihanJawaban2);
         mrbPilihanJawaban3 = findViewById(R.id.rbPilihanJawaban3);
         ImageButton btncekcek = findViewById(R.id.btncekcek);
+        Dialogku = new Dialog(this);
+        Dialognya = new Dialog(this);
+        Dialognya.setContentView(R.layout.dialogslh);
+        Dialogku.setContentView(R.layout.dialogbenar);
         tvKuis = findViewById(R.id.tvResultKuis);
+        updatePertanyaan(r.nextInt(pertanyaanlength));
         ImageButton btnKuis = findViewById(R.id.btnKuis);
-        btnKuis.setOnClickListener(new View.OnClickListener(){
+        btnKuis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                cekJawaban();
+            }
+
+        });
     }
 
-    public void cekJawaban(){
+    private void cekJawaban(){
         if(mrbPilihanJawaban1.isChecked()){ //jika radio button 1 diklik
             //jika text yang tertulis di radio button tsb = nilai dari var jawaban
             if(mrbPilihanJawaban1.getText().toString().equals(jawaban)){
                 Toast.makeText(this, "Jawaban Benar", Toast.LENGTH_SHORT).show(); //keluar notifikasi "Jawaban Benar"
-                setKonten();
+                kusi = Dialogku.findViewById(R.id.kusi);
+                kusi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialogku.dismiss();
+                        Intent goToX = new Intent(QuizActivity.this,MainMenuActivity.class);
+                        startActivity(goToX);
+                    }
+                });
+                Dialogku.getWindow();
+                Dialogku.show();
             }else{
                 Toast.makeText(this, "Jawaban Salah", Toast.LENGTH_SHORT).show();
-                setKonten();
+                tutup = Dialognya.findViewById(R.id.tutup);
+                tutup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialogku.dismiss();
+                        Intent goToX = new Intent(QuizActivity.this,MainMenuActivity.class);
+                        startActivity(goToX);
+                    }
+                });
+                Dialogku.getWindow();
+                Dialogku.show();
             }
         }else if(mrbPilihanJawaban2.isChecked()){
             //jika text yang tertulis di radio button tsb = nilai dari var jawaban
             if(mrbPilihanJawaban2.getText().toString().equals(jawaban)){
-                Toast.makeText(this, "Jawaban Benar", Toast.LENGTH_SHORT).show(); //keluar notifikasi "Jawaban Benar"
-                setKonten(); //update next konten
+                Toast.makeText(this, "Jawaban Benar", Toast.LENGTH_SHORT).show();
+                Dialogku.setContentView(R.layout.dialogbenar);
+                kusi = Dialogku.findViewById(R.id.kusi);
+                kusi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialogku.dismiss();
+                        Intent goToX = new Intent(QuizActivity.this,MainMenuActivity.class);
+                        startActivity(goToX);
+                    }
+                });
+                Dialogku.getWindow();
+                Dialogku.show();
             }else{
                 Toast.makeText(this, "Jawaban Salah", Toast.LENGTH_SHORT).show();
-                setKonten();
+                Dialogku.setContentView(R.layout.dialogbenar);
+                kusi = Dialogku.findViewById(R.id.kusi);
+                kusi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialogku.dismiss();
+                        Intent goToX = new Intent(QuizActivity.this,MainMenuActivity.class);
+                        startActivity(goToX);
+                    }
+                });
+                Dialogku.getWindow();
+                Dialogku.show();
             }
         }else if(mrbPilihanJawaban3.isChecked()){
             //jika text yang tertulis di radio button tsb = nilai dari var jawaban
             if(mrbPilihanJawaban3.getText().toString().equals(jawaban)){
-                Toast.makeText(this, "Jawaban Benar", Toast.LENGTH_SHORT).show(); //keluar notifikasi "Jawaban Benar"
-                setKonten(); //update next konten
+                Toast.makeText(this, "Jawaban Benar", Toast.LENGTH_SHORT).show();
+                Dialogku.setContentView(R.layout.dialogbenar);
+                kusi = Dialogku.findViewById(R.id.kusi);
+                kusi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialogku.dismiss();
+                        Intent goToX = new Intent(QuizActivity.this,MainMenuActivity.class);
+                        startActivity(goToX);
+                    }
+                });
+                Dialogku.getWindow();
+                Dialogku.show();
             }else{
                 Toast.makeText(this, "Jawaban Salah", Toast.LENGTH_SHORT).show();
-                setKonten();
             }
         }else{
-            Toast.makeText(this, "Mau Kemana ?, ayo pilih jawaban terlebih dahulu sebelum jalan", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Silahkan pilih jawaban dulu!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    //ini adalah method bawaan dari Android Studio
-    //fungsi : memberi aksi ketika tombol back pada hp diklik
     public void onBackPressed(){
-        Toast.makeText(this, "Selesaikan Pertanyaan sebelum melanjutkan permainan", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Selesaikan kuis", Toast.LENGTH_SHORT).show();
+    }
+    private void updatePertanyaan(int num) {
+        tvKuis.setText(pertanyaan.getPertanyaan(num));
+        mrbPilihanJawaban1.setText(pertanyaan.getPilihanJawaban1(num));
+        mrbPilihanJawaban2.setText(pertanyaan.getPilihanJawaban2(num));
+        mrbPilihanJawaban3.setText(pertanyaan.getPilihanJawaban3(num));
+        jawaban = pertanyaan.getJawabanBenar(num);
     }
 
-    private void setKonten() {
-    }
 }
-
